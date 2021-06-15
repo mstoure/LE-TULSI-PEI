ca77c4ff462731dbd2d2af785fa1e5d2b8e3950d
mstoure <seyba8.pro@gmail.com>
2021-06-15 18:00:56 +0200

    LE TULSI PEÏ

---
+++ le_tulsi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for le_tulsi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'le_tulsi.settings')
+
+application = get_asgi_application()
---
+++ pei/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PeïConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'peï'
